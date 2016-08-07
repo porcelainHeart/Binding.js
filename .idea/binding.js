24c6f985b020f8700f47a774ee334c320c591947
a982503660 <982503660@qq.com>
2016-08-07 16:37:18 +0800

    添加了注释 方便阅读

--- .idea/binding.js
+++ .idea/binding.js
@@ -5,17 +5,22 @@
 (function () {
     window.binding = function(col, b, c) {
 
+        // 如果col是字符串，包装成数组
         if (typeof(col) === 'string') {
             col = [col];
         }
+
+        // 返回事件对象数组，方便以后调用
         return Array.prototype.slice.call(col, 0).map(function(a) {
 
+            // 参数b初始化
             var watch         = (typeof(b) === 'object' && b.watch)         ? b.watch       : 'value',
                 change        = (typeof(b) === 'object' && b.change)        ? b.change      : 'textContent',
                 defaultValue  = (typeof(b) === 'object' && b.defaultValue)  ? b.defaultValue: false,
                 callback      = (typeof(b) === 'function')                  ? b             : c,
                 pur, inputs, event, modArr = [], conArr = [];
 
+            // 定义构造函数BR，用于生成事件对象
             var BR = function() {
 
                 if (typeof(a) === 'string') {
@@ -28,20 +33,24 @@
                 makeInputs();
             };
 
+            // 获取nodeList
             function takeList(arr) {
                 return document.querySelectorAll(arr.join(','));
             }
 
+            // 新建控制器
             function newController(con) {
                 conArr.push('[data-controller="' + con + '"]');
                 inputs = takeList(conArr);
             }
 
+            // 创建model
             function newModel(mod) {
                 modArr.push('[data-model="' + mod + '"]');
                 pur = takeList(modArr);
             }
 
+            // 为每个输入控件添加事件监听器，句柄发生改变时将触发回调函数handleChange
             function makeInputs() {
                 for (var i = 0; i < inputs.length; i++) {
                     event = (inputs[i].type === 'radio' || inputs[i].type === 'checkbox' || inputs[i].type.indexOf('select') !== -1) ? 'change': 'input';
@@ -49,6 +58,7 @@
                 }
             }
 
+            // 定义监听器回调函数，改变显示的值
             function handleChange() {
                 for (var i = 0; i < pur.length; i++) {
                     var value = this[watch].toString().trim();
@@ -70,6 +80,7 @@
                 }
             }
 
+            // 在BR.prototype上添加方法，作为暴露出去的api接口
             BR.prototype.controllers = conArr;
 
             BR.prototype.models = modArr;
